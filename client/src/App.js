40e5a2eff50aad0307150a84e78a59d6e2c34f24
alisarogers <arogers@ucsd.edu>
2019-02-02 15:54:42 -0800

    front end is ready! waiting to be linked to back end

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,34 @@ import './App.css';
 import { NavTabs } from './NavBar/TabContainer';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
 
 class App extends Component {
   render() {
     return (
+      <div>
      <Grid container
+        xs={16}
+        direction="column"
         justify="center" 
         alignContent="center" 
-        alignItems="center">
+        alignItems="center"
+        spacing="8"
+        className="center"
+        >
        <Grid 
         item xs={8}
-        className="center"
          >
          <Paper >
             <NavTabs />
+            
           </Paper> 
         </Grid>
+        
       </Grid>
+     
+      </div>
     );
   }
 }
---
+++ client/src/Buttons/FormMessage.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default class FormMessage extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="pr#61dafb" onClick={this.handleClickOpen}  > + Message  </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Add Message</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please enter Message to be added here.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Message"
+              type="message"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Add 
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
