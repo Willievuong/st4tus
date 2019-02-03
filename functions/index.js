5be22057c3c19c2c4e72bfc3f3d99519bdb421de
dukepham <32912812+dukepham@users.noreply.github.com>
2019-02-03 04:14:21 -0800

    SAVE POINT

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 exports.getUser = functions.https.onRequest((request, response) => {
+    
    
     //var result = db.collection('users').doc('user1');
     //var result = db.collection('users').doc();
@@ -12,6 +13,7 @@ exports.getUser = functions.https.onRequest((request, response) => {
     var chores = []
     var messages = []
     var result = null
+    var arrayName = []
     db.collection('users').get()
     .then((snapshot) => 
     {
@@ -19,6 +21,7 @@ exports.getUser = functions.https.onRequest((request, response) => {
       {
           //console.log("Check spot 1")
         console.log(doc.id, '=>', doc.data().name);
+        arrayName.push(doc.data().name);
       IDs.push(doc.id)
       //console.log("THE ARRAY: ", IDs);
       });
@@ -37,43 +40,45 @@ exports.getUser = functions.https.onRequest((request, response) => {
         // })
         
         var result = IDs
-        response.send(result);
+        //response.send(result);
         return null
     }).catch(err => {
         console.log("Error", err)
     })
-//     .then((empty) => { 
-//         console.log("Check spot 4")
-//         for(var i = 0; i < IDs.length; i++) {
+    .then((empty) => { 
+        console.log("Check spot 4")
+        //console.log(collection.data().name)
+        for(var i = 0; i < IDs.length; i++) {
 
-//             db.collection('users').doc(IDs[i]).collection('chores').get().then((snapshot) => {
-//                 snapshot.forEach((collection) => {
-//                     chores.push(collection.data().name)
-//                     console.log("Check spot 5")
-                    
-//                 })
-//                 db.collection('users').doc(IDs[i]).collection('messages').get().then((snapshot) => {
-//                 snapshot.forEach((collection) => {
-//                     console.log("Check spot 6")
-//                     messages.push(collection.data().name)
+            db.collection('users').doc(IDs[i]).collection('chores').get().then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    chores.push(doc.data().chore)
+                    console.log(doc.data().chore)
+                })
                 
-//                 }).catch((err) => {console.log(err)})
-//                 console.log("Check spot 7")
-//             }).catch((err) => {console.log(err)})
+                console.log("Check spot 7")
+            }).catch((err) => {console.log(err)})
  
-                
-//         })}
-
-//         var finalResult = {
-//             IDS, 
-//             chores, 
-//             messages,
-//         }
-//         response.send(finalResult)
-//     }).catch( (err) => {
-//         console.log("Error", err)
-// });
-
+            db.collection('users').doc(IDs[i]).collection('messages').get().then((snapshot) => {
+                snapshot.forEach( doc => {
+                    console.log("Check spot 6")
+                    messages.push(doc.data().message)
+                    //console.log(doc.data().message)
+                })  
+            }).catch( err => {console.log(err)})
+            var finalResult = {
+                IDs, 
+                chores, 
+                messages,
+            }
+                //response.send(finalResult)
+            
+    
+        }
+        
+    }).catch( err => {
+        console.log("Error", err)
+    });
 });
 
 
